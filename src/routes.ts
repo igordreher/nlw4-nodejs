3698a0686e198aa039771875989259cfd732bdbd
igordreher <igord.sparrenberger@gmail.com>
2021-02-24 11:08:53 -0300

    Add surveysController

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import surveyController from './controllers/surveyController';
 import userController from './controllers/userController';
 
 const router = Router();
 
 router.post('/users', userController.create);
+
+router.post('/surveys', surveyController.create);
+router.get('/surveys', surveyController.show);
 
 export default router;
